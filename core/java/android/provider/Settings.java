39159550a328e50b007a06c266adedf92fb2278c
Jmz <jmztaylor@gmail.com>
2014-01-06 15:30:15 -0500

    Add support for force expanded notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4022,6 +4022,12 @@ public final class Settings {
         public static final String SHOW_TILE_SUMMARY = "show_tile_summary";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
