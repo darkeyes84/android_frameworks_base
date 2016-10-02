5b5826da9bdb354b2713db503098908d06ec6472
Pranav Vashi <neobuddy89@gmail.com>
2016-10-02 20:23:07 +0530

    Settings: Disable suggestions [2/3]
    Change-Id: I69f0a61624ce5dcb5e8a685c13af7b6747581483
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3831,6 +3831,12 @@ public final class Settings {
         public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
 
         /**
+         * Disable suggestions on the settings dashboard
+         * @hide
+         */
+        public static final String DISABLE_SETTINGS_SUGGESTIONS = "disable_settings_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
