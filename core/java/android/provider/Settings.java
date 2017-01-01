981f393cbd5d39ff1b899186f73f69a3bec2cf1d
ezio84 <brabus84@gmail.com>
2016-12-31 22:50:55 -0500

    Night Light brightness mode options [1/2] Squash of: *Night tile: automatically set a lower brightness when active [1/2] *Night mode tile brightness: allow to set a different value [1/2] *Night light tile brightness: merge the two options into one [1/2] *Night light auto low brightness: wider action [1/2] *Fix manual brightness going to 0 after reboot
    Change-Id: I778d2b9e4d3dc0248e1a9cbeebacfacedbbc2f09

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6803,6 +6803,12 @@ public final class Settings {
         public static final String DEVICE_PAIRED = "device_paired";
 
         /**
+         * Low brightness level for the advanced night mode tile
+         * @hide
+         */
+        public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
+
+        /**
          * Whether to include options in power menu for rebooting into recovery or bootloader
          * @hide
          */
