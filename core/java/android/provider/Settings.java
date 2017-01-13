aed830117564738ad824450f08264e4a9df47569
ezio84 <brabus84@gmail.com>
2017-01-13 15:48:46 +0100

    Night auto brightness: restore user brightness values also after reboot
    if the user enables night mode, so the brightness is auto set to the wanted
    value, then he reboots the device, after reboot if the user disables night mode
    again he wants his custom brightness value back (the one he set before the reboot).
    
    Change-Id: I568690bf49e11e286738b0102886f21b83ab0fff

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6809,6 +6809,18 @@ public final class Settings {
         public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
 
         /**
+         * Allows to store the custom user auto brightness value
+         * @hide
+         */
+        public static final String NIGHT_AUTOBRIGHTNESS_USERVALUE = "night_autobrightness_uservalue";
+
+        /**
+         * Allows to store the custom user manual brightness value
+         * @hide
+         */
+        public static final String NIGHT_MANBRIGHTNESS_USERVALUE = "night_manbrightness_uservalue";
+
+        /**
          * Whether to include options in power menu for rebooting into recovery or bootloader
          * @hide
          */
