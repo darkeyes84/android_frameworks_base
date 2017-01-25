4524082bed1dc83e8de067ae9c2abb8dbc2c693f
Nathan Chancellor <natechancellor@gmail.com>
2017-01-25 14:28:19 -0700

    DND tile: Longpress when active to show detail view

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -116,7 +116,7 @@ public class DndTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return ZEN_SETTINGS;
+        return null;
     }
 
     @Override
@@ -136,6 +136,15 @@ public class DndTile extends QSTile<QSTile.BooleanState> {
             showDetail(true);
             int zen = Prefs.getInt(mContext, Prefs.Key.DND_FAVORITE_ZEN, Global.ZEN_MODE_ALARMS);
             mController.setZen(zen, null, TAG);
+        }
+    }
+
+    @Override
+    public void handleLongClick() {
+        if (mState.value) {
+            showDetail(true);
+        } else {
+            mHost.startActivityDismissingKeyguard(ZEN_SETTINGS);
         }
     }
 
