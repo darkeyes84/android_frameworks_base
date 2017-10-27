731892fe6bc5d71f1d3bb108d4d4c54b12f56d5a
Dark Eyes <dark_eyes_@hotmail.com>
2017-10-27 09:41:19 -0400

    Add switchbar to network traffic
    Change-Id: Ic327442edb14459c39eb43ab7a811502425b3561

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3830,10 +3830,15 @@ public final class Settings {
         public static final String NAV_BAR_DYNAMIC = "nav_bar_dynamic";
 
         /**
+         * Network traffic switch
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_OLD = "network_traffic_old";
+
+        /**
          * Network traffic indicator, goes from least to greatest significant bitwise
-         * 0 = Display up-stream traffic if set
-         * 1 = Display down-stream traffic if set
-         * 2 = Show as Byte/s if set
+         * 0 = Display down-stream traffic if set
+         * 1 = Show as Byte/s if set
          * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
          * @hide
          */
